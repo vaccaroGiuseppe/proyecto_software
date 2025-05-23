2a983a5f110ff2c3ba893fa0bcfdb22b5c6be3e4
LuigiPerezP <perez.luis@correo.unimet.edu.ve>
2025-05-23 14:48:43 -0400

    hola buenas

--- src/Components/Flujogramas/Flujogramas.tsx
+++ src/Components/Flujogramas/Flujogramas.tsx
@@ -2,34 +2,33 @@ import "./Flujogramas.css";
 import { FiDownload } from "react-icons/fi";
 
 function Flujogramas() {
-  // Lista de carreras con sus respectivos enlaces (ejemplo)
   const carreras = [
-    { nombre: "Ingeniería de Sistemas", link: "https://google.com" },
-    { nombre: "Ingeniería Civil", link: "https://yahoo.com" },
-    { nombre: "Ingeniería de Producción", link: "https://bing.com" },
-    { nombre: "Ingeniería Química", link: "https://duckduckgo.com" },
+    { nombre: "Ingeniería de Sistemas", link: "https://docs.google.com/spreadsheets/d/1aa4OponlNMNCY1-9_A8ACP1EaQJ55eue/edit?usp=sharing&ouid=115070425939654893584&rtpof=true&sd=true" },
+    { nombre: "Ingeniería Civil", link: "https://docs.google.com/spreadsheets/d/1SCMRR0vIbrGgFN6qJgmRKI_d3YuwqmNf/edit?usp=sharing&ouid=115070425939654893584&rtpof=true&sd=true" },
+    { nombre: "Ingeniería de Producción", link: "https://docs.google.com/spreadsheets/d/1o97owsDH0hSvrEj8Zqfh686cMpUCC2j-/edit?usp=share_link&ouid=115070425939654893584&rtpof=true&sd=true" },
+    { nombre: "Ingeniería Química", link: "https://docs.google.com/spreadsheets/d/1o97owsDH0hSvrEj8Zqfh686cMpUCC2j-/edit?usp=share_link&ouid=115070425939654893584&rtpof=true&sd=true" },
     { nombre: "Derecho", link: "https://wikipedia.org" },
-    { nombre: "Ciencias Administrativas (Ambas Menciones)", link: "https://microsoft.com" },
+    { nombre: "Ciencias Administrativas (Ambas Menciones)", link: "https://docs.google.com/spreadsheets/d/1FkctKvPGYNRTO5eda298t4xb5WQ7Yts6/edit?usp=share_link&ouid=115070425939654893584&rtpof=true&sd=true" },
     { nombre: "Psicología", link: "https://apple.com" },
     { nombre: "Comunicación Social y Empresarial", link: "https://amazon.com" },
-    { nombre: "Contaduría Pública", link: "https://twitter.com" },
-    { nombre: "Economía Empresarial", link: "https://facebook.com" },
-    { nombre: "Educación, Mención Educación Inicial", link: "https://instagram.com" },
-    { nombre: "Educación, Mención Gerencia", link: "https://linkedin.com" },
+    { nombre: "Contaduría Pública", link: "https://docs.google.com/spreadsheets/d/1p9r7hYLdzz1Gbr11wLNKyR4bSXFrQNHS/edit?usp=share_link&ouid=115070425939654893584&rtpof=true&sd=true" },
+    { nombre: "Economía Empresarial", link: "https://docs.google.com/spreadsheets/d/1FkctKvPGYNRTO5eda298t4xb5WQ7Yts6/edit?usp=share_link&ouid=115070425939654893584&rtpof=true&sd=true" },
+    { nombre: "Educación, Mención Educación Inicial", link: "https://docs.google.com/spreadsheets/d/1MZFG6wxIzVi3LA_CRuhnrsjJwvDxA0Ja/edit?usp=share_link&ouid=115070425939654893584&rtpof=true&sd=true" },
+    { nombre: "Educación, Mención Gerencia", link: "https://docs.google.com/spreadsheets/d/1415m6pwcI6u5IX5yTNLULfhP-dFejURY/edit?usp=share_link&ouid=115070425939654893584&rtpof=true&sd=true" },
     { nombre: "Estudios Liberales", link: "https://reddit.com" },
     { nombre: "Estudios Internacionales, mención Derecho Internacional", link: "https://netflix.com" },
     { nombre: "Estudios Internacionales, mención Economía Internacional", link: "https://youtube.com" },
-    { nombre: "Idiomas Modernos", link: "https://github.com" },
-    { nombre: "Ingeniería Eléctrica", link: "https://stackoverflow.com" },
-    { nombre: "Ingeniería Mecánica", link: "https://medium.com" },
+    { nombre: "Idiomas Modernos", link: "https://docs.google.com/spreadsheets/d/1UTrqHfAbiz1n0KqTcisthvdDoin82ndw/edit?usp=share_link&ouid=115070425939654893584&rtpof=true&sd=true" },
+    { nombre: "Ingeniería Eléctrica", link: "https://docs.google.com/spreadsheets/d/14LCIF-rM-NaksU-ZBMJuSD_FNDiZSiu3/edit?usp=sharing&ouid=115070425939654893584&rtpof=true&sd=true" },
+    { nombre: "Ingeniería Mecánica", link: "https://docs.google.com/spreadsheets/d/1VloXKOJuuq-p8TVqHY7z1E8BwMDh2032/edit?usp=sharing&ouid=115070425939654893584&rtpof=true&sd=true" },
     { nombre: "Idiomas Modernos", link: "https://wordpress.com" },
-    { nombre: "Matemáticas Industriales", link: "https://dropbox.com" }
+    { nombre: "Matemáticas Industriales", link: "https://docs.google.com/spreadsheets/d/1jHbwynhmpgv9Q6HN0IigQe4elwz-szqd/edit?usp=share_link&ouid=115070425939654893584&rtpof=true&sd=true" }
   ];
 
   return (
     <div className="Flujograma_Contenedor">
       <div className="Contenedor_Titulo">
-        <h1 className="Titulo">Flujogramas</h1>
+        <h1 className="Titulo">Flujogramas Interactivos</h1>
       </div>
 
       <div className="Carreras_Grid">
@@ -37,8 +36,8 @@ function Flujogramas() {
           <a
             key={index}
             href={carrera.link}
-            target="_blank"  // Abre en nueva pestaña
-            rel="noopener noreferrer"  // Seguridad para target="_blank"
+            target="_blank" 
+            rel="noopener noreferrer"
             className="Carrera_Item"
           >
             <FiDownload className="Icono_Descarga" />
